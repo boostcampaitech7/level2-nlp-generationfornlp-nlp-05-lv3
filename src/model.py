01c64dea3cdeb9261578e8a00c138e4cd1b2ea8e
gwaksital <y10cuk@naver.com>
2024-11-21 02:01:51 +0000

    GENNLP-29: refactor: Enable configurations changes via config.yaml and integrate unsloth

--- src/model.py
+++ src/model.py
@@ -5,18 +5,107 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-from peft import AutoPeftModelForCausalLM
-from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 
-from src.utils import get_peft_config, get_sft_config, get_quant_config
+from src.utils import get_sft_config
+from unsloth import FastLanguageModel
+from peft import AutoPeftModelForCausalLM, LoraConfig
+
+
+def formatting_prompts_func(example, tokenizer):
+    output_texts = []
+    for message in example["messages"]:
+        output_texts.append(
+            tokenizer.apply_chat_template(
+                message,
+                tokenize=False,
+            )
+        )
+    return output_texts
+
+def tokenize_function(element, tokenizer):
+    outputs = tokenizer(
+        formatting_prompts_func(element, tokenizer),
+        truncation=False,
+        padding=False,
+        return_overflowing_tokens=False,
+        return_length=False,
+    )
+    return {
+        "input_ids": outputs["input_ids"],
+        "attention_mask": outputs["attention_mask"],
+    }
+
+def tokenize_dataset(dataset, tokenizer):
+    tokenized_dataset = dataset.map(
+        lambda element: tokenize_function(element, tokenizer),  # tokenizer 전달
+        remove_columns=list(dataset.features),
+        batched=True,
+        num_proc=4,  # 병렬 처리
+        load_from_cache_file=True,
+        desc="Tokenizing",
+    )
+    return tokenized_dataset
+
 
 
 class MyModel():
-    def __init__(self, config):
+    def __init__(self, config, mode):
         self.config = config
         self.peft_c = config['peft']
         self.model_c = config['model']
+
+        if mode == "train":
+            self.model, self.tokenizer = FastLanguageModel.from_pretrained(
+                model_name=config['model']['train']['train_model_name'],
+                max_seq_length=config['model']['max_seq_length'],
+                dtype=getattr(torch, config['model']['torch_dtype']),
+                load_in_4bit=config['model']['load_in_4bit']
+            )
+            
+            self.model = FastLanguageModel.get_peft_model(
+                self.model,
+                r=self.peft_c['r'],
+                lora_alpha=self.peft_c['lora_alpha'],
+                lora_dropout=self.peft_c['lora_dropout'],
+                target_modules=self.peft_c['target_modules'],
+                bias=self.peft_c['bias'],
+                use_gradient_checkpointing=self.peft_c['use_gradient_checkpointing'],
+                random_state=self.peft_c['random_state'],
+                use_rslora=self.peft_c['use_rslora'],
+                loftq_config=self.peft_c['loftq_config'],
+            )
+
+        elif mode == "test":
+            self.model = AutoPeftModelForCausalLM.from_pretrained(
+                self.model_c['test']['test_checkpoint_path'],
+                trust_remote_code=True,
+                device_map="auto",
+            )            
+            self.tokenizer = AutoTokenizer.from_pretrained(
+                self.model_c['test']['test_checkpoint_path'],
+                trust_remote_code=True,
+            )
+        
+        # gemma-ko-2b에 chat template 직접 지정
+        if self.model_c["chat_template"]:
+            self.tokenizer.chat_template = (
+                "{% if messages[0]['role'] == 'system' %}"
+                "{% set system_message = messages[0]['content'] %}"
+                "{% endif %}"
+                "{% if system_message is defined %}"
+                "{{ system_message }}"
+                "{% endif %}"
+                "{% for message in messages %}"
+                "{% set content = message['content'] %}"
+                "{% if message['role'] == 'user' %}"
+                "{{ '<start_of_turn>user\n' + content + '<end_of_turn>\n<start_of_turn>model\n' }}"
+                "{% elif message['role'] == 'assistant' %}"
+                "{{ content + '<end_of_turn>\n' }}"
+                "{% endif %}"
+                "{% endfor %}"
+            )
 
         # metric 로드
         self.acc_metric = evaluate.load("accuracy")
@@ -26,78 +115,25 @@ class MyModel():
         self.pred_choices_map = {0: "1", 1: "2", 2: "3", 3: "4", 4: "5"}
     
     def tokenize(self, processed_train):
-        tokenizer = self.tokenizer
+        tokenized = tokenize_dataset(processed_train, self.tokenizer)
 
-        def tokenize_fn(element):
-            output_texts = []
-            for i in range(len(element["messages"])):
-                output_texts.append(
-                    tokenizer.apply_chat_template(
-                        element["messages"][i],
-                        tokenize=False,
-                    )
-                )
-
-            outputs = tokenizer(
-                output_texts,
-                truncation=False,
-                padding=False,
-                return_overflowing_tokens=False,
-                return_length=False
-            )
-
-            return {
-                "input_ids": outputs["input_ids"],
-                "attention_mask": outputs["attention_mask"],
-            }
-
-        tokenized = processed_train.map(
-            tokenize_fn,
-            remove_columns=list(processed_train.features),
-            batched=True,
-            num_proc=4,
-            load_from_cache_file=True,
-            desc="Tokenizing"
-        )
-
-        # vram memory 제약으로 인해 인풋 데이터의 길이가 1024 초과인 데이터는 제외하였습니다.
-        # *힌트: 1024보다 길이가 더 긴 데이터를 포함하면 더 높은 점수를 달성할 수 있을 것 같습니다!
-        tokenized = tokenized.filter(lambda x: len(x["input_ids"]) <= 1024)
-        # validation 데이터셋 고정할 수도 있을 것
-        tokenized = tokenized.train_test_split(test_size=0.1, seed=42)
-
-        self.train_dataset = tokenized["train"]
-        self.eval_dataset = tokenized["test"]
+        tokenized = tokenized.filter(lambda x: len(x["input_ids"]) <= self.model_c['max_seq_length'])
+        
+        if self.model_c['train_valid_split']:
+            tokenized = tokenized.train_test_split(test_size=0.1, seed=42)
+            print("*"*50)
+            print("Log: Train Vaild 9:1로 나눴음")
+            print("*"*50)
+            self.train_dataset = tokenized["train"]
+            self.eval_dataset = tokenized["test"]
+        else:
+            print("*"*50)
+            print("Log: Train Vaild 안나눴음")
+            print("*"*50)
+            self.train_dataset = tokenized
+            self.eval_dataset = None
 
     def train(self, processed_train):
-        quant_config = get_quant_config(self.config['quantization'])
-
-        if self.model_c["torch_dtype"] == "float16":
-            dtype = torch.float16
-        elif self.model_c["torch_dtype"] == "float32":
-            dtype = torch.float32
-        
-        self.model = AutoModelForCausalLM.from_pretrained(
-            self.model_c['name_or_path'],
-            torch_dtype=dtype,
-            trust_remote_code=True,
-            quantization_config=quant_config,
-            device_map="auto"
-        )
-
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.model_c['name_or_path'],
-            trust_remote_code=True,
-        )
-
-        if self.model_c["chat_template"]:
-            self.tokenizer.chat_template = "{% if messages[0]['role'] == 'system' %}{% set system_message = messages[0]['content'] %}{% endif %}{% if system_message is defined %}{{ system_message }}{% endif %}{% for message in messages %}{% set content = message['content'] %}{% if message['role'] == 'user' %}{{ '<start_of_turn>user\n' + content + '<end_of_turn>\n<start_of_turn>model\n' }}{% elif message['role'] == 'assistant' %}{{ content + '<end_of_turn>\n' }}{% endif %}{% endfor %}"
-        
-        # pad token 설정
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
-        self.tokenizer.padding_side = 'right'
-
         self.tokenize(processed_train)
 
         response_template = "<start_of_turn>model"
@@ -129,38 +165,33 @@ class MyModel():
             acc = self.acc_metric.compute(predictions=predictions, references=labels)
             return acc
 
-        peft_config = get_peft_config(self.peft_c)
-        sft_config = get_sft_config(self.model_c)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+        self.tokenizer.padding_side = 'right'
+        
+        do_eval = self.eval_dataset is not None
+        sft_config = get_sft_config(self.config, do_eval=do_eval)
 
-        trainer = SFTTrainer(
-            model=self.model,
-            train_dataset=self.train_dataset,
-            eval_dataset=self.eval_dataset,
-            data_collator=data_collator,
-            tokenizer=self.tokenizer,
-            compute_metrics=compute_metrics,
-            preprocess_logits_for_metrics=preprocess_logits_for_metrics,
-            peft_config=peft_config,
-            args=sft_config
-        )
+        trainer_args = {
+            "model": self.model,
+            "train_dataset": self.train_dataset,
+            "data_collator": data_collator,
+            "tokenizer": self.tokenizer,
+            "args": sft_config
+        }
+        
+        if do_eval:
+            trainer_args["eval_dataset"] = self.eval_dataset
+            trainer_args["compute_metrics"] = compute_metrics
+            trainer_args["preprocess_logits_for_metrics"] = preprocess_logits_for_metrics
+
+        trainer = SFTTrainer(**trainer_args)
 
         trainer.train()
     
     def inference(self, processed_test, output_dir):
-        self.model = AutoPeftModelForCausalLM.from_pretrained(
-            self.model_c["name_or_path"],
-            trust_remote_code=True,
-            torch_dtype=torch.bfloat16,
-            device_map="auto",
-        )
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.model_c["name_or_path"],
-            trust_remote_code=True,
-        )
-
         infer_results = []
 
-        self.model.to("cuda")
         self.model.eval()
         with torch.inference_mode():
             for data in tqdm(processed_test):
@@ -190,4 +221,4 @@ class MyModel():
                 predict_value = self.pred_choices_map[np.argmax(probs, axis=-1)]
                 infer_results.append({"id": _id, "answer": predict_value})
         
-        pd.DataFrame(infer_results).to_csv(os.path.join(output_dir, "output.csv"), index=False)
+        pd.DataFrame(infer_results).to_csv(output_dir, index=False)
